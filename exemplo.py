a92c58ed592ae915ebd87296f5b123f8f7f9713e
André <108902451+DreZitus@users.noreply.github.com>
2025-11-07 01:31:07 +0000

    nova lista com dados de preço e quantidade

--- exemplo.py
+++ exemplo.py
@@ -1,2 +1,8 @@
 lista_de_compras = ["arroz", "feijão", "carne", "tomate"] #informação guardada na lista
 
+
+compras_efetuadas = [   #informações do preço e quantidade
+    ("arroz", 3.49, 3),
+    ("feijão, 2,99, 2"),
+    ("tomate", 1.99, 9.8)
+]
