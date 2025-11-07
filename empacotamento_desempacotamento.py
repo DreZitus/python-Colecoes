0350e71c48f471e91a6b703132aee997a0f28ba1
André <108902451+DreZitus@users.noreply.github.com>
2025-11-07 01:54:42 +0000

    adicionando condicional para desempacotamento com tuplas

--- empacotamento_desempacotamento.py
+++ empacotamento_desempacotamento.py
@@ -1,1 +1,14 @@
 #empacotamento com tuplas
+
+nome_produto = "carne"
+preco = 22.9
+qtde_comprada = 1
+
+carne = (nome_produto, preco, qtde_comprada)
+
+#desempacotamento com tuplas
+
+compras_efetuadas = ""
+
+for nome, preco, qtde in compras_efetuadas:
+    print(f"nome: {nome}\tPreço: {preco}\tQtde: {qtde}")
